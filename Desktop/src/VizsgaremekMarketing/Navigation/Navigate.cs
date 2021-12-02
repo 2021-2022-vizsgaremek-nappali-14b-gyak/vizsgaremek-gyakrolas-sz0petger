3db046e75e29843bba2ce291afedea8e0e30df73
péter gergely <petergergo12345@gmail.com>
2021-12-02 09:41:47 +0100

    Félbehagyott adatok

---
+++ Desktop/src/VizsgaremekMarketing/Navigation/Navigate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VizsgaremekMarketing.Navigation
+{
+    class Navigate
+    {
+        private static MainWindow mainwindow;
+
+        public static void  Navigation(UserControl userControl)
+        {
+            mainwindow.PageContent.Children.Add(userControl)
+        }
+    }
+}
