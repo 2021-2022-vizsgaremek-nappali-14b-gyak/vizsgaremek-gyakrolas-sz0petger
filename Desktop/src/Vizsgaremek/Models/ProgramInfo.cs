79dc8a7c5c4f7d60ce12384ae332a305ce096b5f
péter gergely <petergergo12345@gmail.com>
2022-01-06 08:14:16 +0100

    uptodate

---
+++ Desktop/src/Vizsgaremek/Models/ProgramInfo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+using Octokit;
+using System.Diagnostics;
+
+namespace Vizsgaremek.Models
+{
+    public class ProgramInfo
+    {
+        private Version version;
+        private string authors;
+        private string title;
+        private string description;
+        private string company;
+
+        public Version Version
+        {
+            get
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var assemblyVersion = assembly.GetName().Version;
+                return assemblyVersion;
+            }
+        }
+
+        public string Authors
+        {
+            get
+            {
+                return authors;                
+            }
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string Company { get => company; set => company = value; }
+
+        public ProgramInfo()
+        {
+            GetGithubCollaboratorsName();
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            foreach (Attribute attr in Attribute.GetCustomAttributes(assembly))
+            {
+                if (attr.GetType() == typeof(AssemblyTitleAttribute))
+                    Title = ((AssemblyTitleAttribute)attr).Title;
+                else if (attr.GetType() == typeof(AssemblyDescriptionAttribute))
+                    Description = ((AssemblyDescriptionAttribute)attr).Description;
+                else if (attr.GetType() == typeof(AssemblyCompanyAttribute))
+                    Company = ((AssemblyCompanyAttribute)attr).Company;
+
+            }
+        }
+
+        private async void GetGithubCollaboratorsName()
+        {
+            string reponame = "gycsaba-vasvari";
+            int repoId = 431763351;
+            var client = new GitHubClient(new ProductHeaderValue(reponame));
+
+            // fejlesztők meghatározása
+            try
+            {
+                var collaborators = await client.Repository.GetAllContributors(repoId);
+                string collaboratorsName = string.Empty;
+                foreach (var collaborator in collaborators)
+                {
+                    string collaboratorLoginName = collaborator.Login;
+                    var user = await client.User.Get(collaboratorLoginName);
+                    collaboratorsName += user.Name + " (" + user.Login + ") ";
+                }
+                authors = collaboratorsName;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
--- Desktop/src/Vizsgaremek/Pages/WelcomePage.xaml.cs
+++ Desktop/src/Vizsgaremek/Pages/WelcomePage.xaml.cs
@@ -12,24 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using VizsgaremekMarketing.Navigation;
-using VizsgaremekMarketing.Pages;
 
-namespace VizsgaremekMarketing
+namespace Vizsgaremek.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WelcomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WelcomePage : UserControl
     {
-        public MainWindow()
+        public WelcomePage()
         {
             InitializeComponent();
-            Navigate.mainwindow = this;
-
-            WelcomePage welcomepage = new WelcomePage();
-            Navigate.Navigation(welcomepage);
-            
         }
     }
 }
